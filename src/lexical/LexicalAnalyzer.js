e7a8bbf499bfadd75b43cd3328148d4fb64a660c
chengui <1372933848@qq.com>
2020-12-09 21:42:56 +0800

    词法分析器添加正则表达式

--- src/lexical/LexicalAnalyzer.js
+++ src/lexical/LexicalAnalyzer.js
@@ -2,14 +2,20 @@
 class LexicalAnalyzer{
     WordType = ["KEYWORD", "IDENTIFIER", "CONSTANT", "OPERATOR", "DELIMITER", "ERROR"]
     Keyword = ["int", "void", "if", "else", "while", "return"]
-    ConstType = ["INTEGER"]
-    Operator = ["+", "-", "*", "/", "=", "==", ">", ">=", "<", "<=", "!="]
+    ConstType = ["INTEGER", "FLOAT", "DOUBLE", "STRING"]
+    Operator = ["+", "-", "*", "/", "<", "<=", "=", "==", ">", ">=", "!="]
     Delimiter = [",", ";", "(", ")", "{", "}", "//", "/*", "*/", "#"]
-    ErrorType = ["IDENTIFIER", "CONSTANT", "OPERATOR", "DELIMITER", "UNKNOWN", "END"]
-    code = ""
-    codeIndex = 0
-    lineCounter = 0
-    lexResult = []
+
+    KeywordRegexp = /^(int|void|if|else|while|return)/
+    IdentifierRegexp = /^([a-zA-Z]+\d*)/
+    NumRegexp = /^(0|[1-9][0-9]*)/
+    OperatorRegexp = /^(\+|-|\*|\/|=|==|>|>=|<|<=|!=)/
+    DelimiterRegexp = /^(,|;|\(|\)|\{|\}|\/\/|\/\*|\*\/|#)/
+
+    code = ""       //代码
+    codeIndex = 0   //代码索引
+    line = 1        //当前词法分析所在行(从'1'开始)
+    col = 1         //当前词法分析所在列(从'1'开始)
 
     /**
      * 初始化词法分析器
@@ -19,8 +25,8 @@ class LexicalAnalyzer{
     initLexAnalyzer(code){
         this.code = code
         this.codeIndex = 0
-        this.lineCounter = 0
-        this.lexResult = []
+        this.line = 1
+        this.col = 1
     }
 
     /**
@@ -53,7 +59,7 @@ class LexicalAnalyzer{
      * @return {bool}
      */
     isOperator(ch){
-        if(this.Operator.toString().includes(ch))
+        if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '<' || ch == '=' || ch == '>' || ch == '!')
             return true
         else
             return false
@@ -65,7 +71,7 @@ class LexicalAnalyzer{
      * @return {bool}
      */
     isBlank(ch){
-        if(ch == ' ' || ch == '\t'|| ch == '\n' || ch == '\r')
+        if(ch == ' ' || ch == '\t'|| ch == '\r' || ch == '\n')
             return true
         else
             return false
@@ -83,9 +89,15 @@ class LexicalAnalyzer{
         else{
             ch = this.code[this.codeIndex]
             this.codeIndex++
-            if(ch == '\n')
-                this.lineCounter++
         }
+        if(ch == '\t')
+            this.col += 6 - this.col % 4
+        else if(ch == '\n'){
+            this.line++
+            this.col = 1
+        }
+        else if(ch != '\r')
+            this.col++
         return ch
     }
 
@@ -98,9 +110,24 @@ class LexicalAnalyzer{
         let ch = ''
         let chNext = ''
         let str = ''
-        let word = {type: "", value: "", line: 0}
+        let word = {
+            type: "",
+            value: "",
+            loc: {
+                start: {
+                    line: 0,
+                    col: 0
+                },
+                end: {
+                    line: 0,
+                    col: 0
+                }
+            }
+        }
 
         while(true){
+            word.loc.start.line = this.line
+            word.loc.start.col = this.col
             ch = this.getNextChar()
             str = ch
             if(ch == '/'){
@@ -108,32 +135,34 @@ class LexicalAnalyzer{
                 if(chNext == '/'){
                     do chNext = this.getNextChar()
                     while((chNext != '\n') && (chNext != '#'))
-                    if(chNext == '#')
+                    if(chNext == '#'){
                         this.codeIndex--
+                        this.col--
+                    }
                 }
                 else if(chNext == '*'){
                     while(true){
                         do chNext = this.getNextChar()
                         while((chNext != '*') && (chNext != '#'))
                         if(chNext == '#'){
-                            this.codeIndex--
-                            word.type = "LexError"
-                            word.value = "DELIMITER ERROR: 缺少界符 \"*/\""
+                            word.type = "ERROR"
+                            word.value = "缺少\"*/\""
                             break
                         }
                         if(this.getNextChar() == '/')
                             break
-                        else
+                        else{
                             this.codeIndex--
+                            this.col--
+                        }
                     }
-                    if(word.type == "LexError")
+                    if(word.type == "ERROR")
                         break
-                }
-                    
+                }        
                 /*除号*/
                 else{
-                    this.codeIndex--
-                    word.type = "OPERATOR"
+                    word.type = "/"
+                    word.value = "/"
                     break
                 }
             }
@@ -143,11 +172,8 @@ class LexicalAnalyzer{
                     str = str.concat(chNext)
                     chNext = this.getNextChar()
                 }
-                this.codeIndex--
-                if(this.Keyword.includes(str)){
+                if(this.Keyword.includes(str))
                     word.type = str
-                    str = "--"
-                }
                 else
                     word.type = "identifier"
                 break
@@ -163,69 +189,60 @@ class LexicalAnalyzer{
                         str = str.concat(chNext)
                         chNext = this.getNextChar()
                     }
-                    this.codeIndex--
-                    word.type = "LexError"
-                    word.value = "IDENTIFIER ERROR: 标识符不能以数字开头!!!"
+                    word.type = "ERROR"
+                    word.value = "标识符或关键字不能紧跟在数字后面!!!"
                 }
-                else{
-                    this.codeIndex--
+                else
                     word.type = "num"
-                }
                 break
             }
             else if(this.isOperator(ch)){
-                if(ch == '=' || ch == '>' || ch == '<' || ch == '!'){
+                if(ch == '<' || ch == '=' || ch == '>' || ch == '!'){
                     chNext = this.getNextChar()
                     if(chNext == '='){
                         str = str.concat(chNext)
                         word.type = str
-                        str = "--"
                     }
                     else{
                         if(ch == '!'){
-                            this.codeIndex--
-                            word.type = "LexError"
-                            word.value = "OPERATOR ERROR: \"!\" 不是操作符!!!"
+                            word.type = "ERROR"
+                            word.value = "\"!\" 不是操作符!!!"
                         }
                         else{
                             this.codeIndex--
+                            this.col--
                             word.type = str
-                            str = "--"
                         }
                     }
                 }
-                else{
+                else
                     word.type = str
-                    str = "--"
-                }
                 break
             }
             else if(this.Delimiter.includes(ch)){
                 word.type = str
-                str = "--"
                 break
             }
             else if(this.isBlank(ch))
                 continue
             else{
                 chNext = this.getNextChar()
-                while((chNext != '/') && !this.isLetter(chNext) && !this.isDigit(chNext) && !this.Operator() && !this.Delimiter() && !this.isBlank()){
+                while((chNext != '/') && !this.isLetter(chNext) && !this.isDigit(chNext) && !this.isOperator(chNext) && !this.Delimiter.includes(chNext) && !this.isBlank()){
                     str = str.concat(chNext)
                     chNext = this.getNextChar()
                 }
-                this.codeIndex--
                 word.type = "ERROR"
-                word.value = "UNKNOWN ERROR: 出现未知的单词!!!"
+                word.value = "出现未知的单词!!!"
                 break
             }
         }
-        word.line = this.lineCounter
-        if(word.type == "LexError"){
-            word.value = str + " " + word.value
-            throw word
+        if(!this.Delimiter.includes(word.type) && !this.isOperator(word.type)){
+            this.codeIndex--
+            this.col--
         }
-        else
-            word.value = str
+        word.loc.end.line = this.line
+        word.loc.end.col = this.col
+        word.value = str
         return word
     }
 
@@ -235,27 +252,222 @@ class LexicalAnalyzer{
      * @return {array}
      */
     async getLexResult(){
-        let word = {type: "", value: "", line: 0}
+        let word = {
+            type: "",
+            value: "",
+            loc: {
+                start: {
+                    line: 0,
+                    col: 0
+                },
+                end: {
+                    line: 0,
+                    col: 0
+                }
+            }
+        }
+        let lexResult = []
         while(this.codeIndex < this.code.length){
             try{
                 word = await this.getNextWord()
                 if((word.type == '#') && (this.codeIndex < this.code.length)){
-                    word.type = "LexError"
-                    word.value = "END ERROR: \"#\" 应该出现在代码结尾处!!!"
-                    throw word
+                    word.type = "ERROR"
+                    word.value = "\"#\" 应该出现在代码结尾处!!!"
                 }
-                this.lexResult.push(word)
+                lexResult.push(word)
+                if(word.type == "ERROR")
+                    break
+                if((word.type != '#') && (this.codeIndex == this.code.length)){
+                    let word2 = {
+                        type: "",
+                        value: "",
+                        loc: {
+                            start: {
+                                line: 0,
+                                col: 0
+                            },
+                            end: {
+                                line: 0,
+                                col: 0
+                            }
+                        }
+                    }
+                    word2.type = "#"
+                    word2.value = "#"
+                    word2.loc.start.line = this.line
+                    word2.loc.start.col = this.col
+                    word2.loc.end.line = this.line
+                    word2.loc.end.col = ++this.col
+                    lexResult.push(word2)
+                }
             }
             catch(err){
                 throw err
             }
         }
-        return this.lexResult
+        return lexResult
+    }
+
+    /**
+     * 通过词法正则表达式得到词法翻译结果
+     * @param
+     * @return {array}
+     */
+    async getLexResultByRegex(){
+        let str = this.code
+        let lexResult = []
+        try{
+            while(str.length > 0){
+                let word = {
+                    type: "",
+                    value: "",
+                    loc: {
+                        start: {
+                            line: 0,
+                            col: 0
+                        },
+                        end: {
+                            line: 0,
+                            col: 0
+                        }
+                    }
+                }
+                while(this.isBlank(str[0])){
+                    if(str[0] == ' '){
+                        str = str.replace(/^ /, "")
+                        this.col++
+                    }
+                    else if(str[0] == '\t'){
+                        str = str.replace(/^\t/, "")
+                        this.col += 6 - this.col % 4
+                    }
+                    else if(str[0] == '\n'){
+                        str = str.replace(/^\n/, "")
+                        this.line++
+                        this.col = 1
+                    }
+                    else
+                        str.replace(/^\t/, "")
+                }
+                word.loc.start.line = this.line
+                word.loc.start.col = this.col
+                if(this.IdentifierRegexp.test(str)){
+                    if(this.KeywordRegexp.test(str) && this.KeywordRegexp.exec(str)[0].length == this.IdentifierRegexp.exec(str)[0].length){
+                        word.type = this.KeywordRegexp.exec(str)[0]
+                        word.value = word.type
+                        this.col += word.value.length
+                        str = str.replace(this.KeywordRegexp, "")
+                    }
+                    else{
+                        word.type = "identifier"
+                        word.value = this.IdentifierRegexp.exec(str)[0]
+                        this.col += word.value.length
+                        str = str.replace(this.IdentifierRegexp, "")
+                    }
+                }
+                else if(this.NumRegexp.test(str)){
+                    word.type = "num"
+                    word.value = this.NumRegexp.exec(str)[0]
+                    this.col += word.value.length
+                    str = str.replace(this.NumRegexp, "")
+                }
+                else if(this.DelimiterRegexp.test(str)){
+                    word.type = this.DelimiterRegexp.exec(str)[0]
+                    word.value = word.type
+                    this.col += word.value.length
+                    str = str.replace(this.DelimiterRegexp,"")
+                    if(word.value[0] == '/' && word.value[1] == '/'){
+                        while(str[0] != '\n' && str[0] != '#' && str.length > 0){
+                            this.col++
+                            if(str.length >= 2)
+                                str = str.substring(1,str.length - 1)
+                            else
+                                str = ""
+                        }
+                        continue
+                    }
+                    else if(word.value[0] == '/' && word.value[1] == '*'){
+                        while(str[0] != '#' && str.length > 1){
+                            if(str[0] == '\t')
+                                this.col += 6 - this.col % 4
+                            else if(str[0] == '\n'){
+                                this.line++
+                                this.col = 1
+                            }
+                            else if(str[0] == '*' && str[1] == '/'){
+                                this.col += 2
+                                if(str.length >= 3)
+                                    str = str.substring(2,str.length - 1)
+                                else
+                                    str = ""
+                                continue
+                            }
+                            else
+                                this.col++
+                            if(str.length >= 2)
+                                str = str.substring(1,str.length - 1)
+                            else
+                                str = ""
+                        }
+                        word.type = "ERROR"
+                        word.value = "缺少\"*/\""
+                    }
+                }
+                else if(this.OperatorRegexp.test(str)){
+                    word.type = this.OperatorRegexp.exec(str)[0]
+                    word.value = word.type
+                    this.col += word.value.length
+                    str = str.replace(this.OperatorRegexp, "")
+                }
+                else{
+                    word.type = "ERROR"
+                    word.value = "出现未知的单词!!!"
+                    this.col ++
+                }
+                word.loc.end.line = this.line
+                word.loc.end.col = this.col
+                if((word.type == '#') && str.length > 0){
+                    word.type = "ERROR"
+                    word.value = "\"#\" 应该出现在代码结尾处!!!"
+                }
+                lexResult.push(word)
+                if(word.type == "ERROR")
+                    break
+                if((word.type != '#') && str.length == 0){
+                    let word2 = {
+                        type: "",
+                        value: "",
+                        loc: {
+                            start: {
+                                line: 0,
+                                col: 0
+                            },
+                            end: {
+                                line: 0,
+                                col: 0
+                            }
+                        }
+                    }
+                    word2.type = "#"
+                    word2.value = "#"
+                    word2.loc.start.line = this.line
+                    word2.loc.start.col = this.col
+                    word2.loc.end.line = this.line
+                    word2.loc.end.col = ++this.col
+                    lexResult.push(word2)
+                }
+            }
+        }
+        catch(err){
+            throw err
+        }
+        return lexResult
     }
 
 }
 
-let lexAnalyzer = new LexicalAnalyzer()
+module.exports = LexicalAnalyzer
+
 code = "\
 int a;\n\
 int b;\n\
@@ -291,19 +503,24 @@ int c;\n\
 a=3;\n\
 b=4;\n\
 c=2;\n\
-a=program(a,b,demo(c))\n\
+a=program(a,b,demo(c));\n\
 return;\n\
-}\n\
+}\
 "
+
+// let lexAnalyzer = new LexicalAnalyzer()
+// code2 = "1"
+
 // lexAnalyzer.initLexAnalyzer(code)
+
 // async function test() {
 //     try{
-//         let lexResult = await lexAnalyzer.getLexResult()
+//         let lexResult = await lexAnalyzer.getLexResultByRegex() 
 //         console.log(lexResult)
 //     }
 //     catch(err){
 //         console.log(err)
 //     }
 // }
+
 // test()
-module.exports = LexicalAnalyzer
