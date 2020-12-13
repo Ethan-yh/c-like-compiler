7a36a603b3fab9dfe365015fed8ed62941b55c0d
chengui <1372933848@qq.com>
2020-12-13 15:47:05 +0800

    小bug4

--- src/lexical/LexicalAnalyzer.js
+++ src/lexical/LexicalAnalyzer.js
@@ -179,6 +179,8 @@ class LexicalAnalyzer{
                 }        
                 /*除号*/
                 else{
+                    this.codeIndex--
+                    this.col--
                     word.type = "/"
                     word.value = "/"
                     break
@@ -539,16 +541,16 @@ return;\n\
 }\
 "
 
-let lexAnalyzer = new LexicalAnalyzer()
-code2 = "int a;\n\
-\r\n \r\n \r\n"
+// let lexAnalyzer = new LexicalAnalyzer()
+// code2 = "/a\
+// \r\n \r\n \r\n"
 
-code3 ="int a;\r\nint b;\r\nint program(int a,int b,int c)\r\n{\r\n\tint i;\r\n\tint j;\r\n\ti=0; \t\r\n\tif(a>(b+c))\r\n\t{\r\n\t\tj=a+(b*c+1);\r\n\t}\r\n\telse\r\n\t{\r\n\t\tj=a;\r\n\t}\r\n\twhile(i<=100)\r\n\t{\r\n\t\ti=j*2;\r\n\t}\r\n\treturn i;\r\n}\r\n \r\n\r\n" 
-lexAnalyzer.initLexAnalyzer(code)
+// code3 ="int a;\r\nint b;\r\nint program(int a,int b,int c)\r\n{\r\n\tint i;\r\n\tint j;\r\n\ti=0; \t\r\n\tif(a>(b+c))\r\n\t{\r\n\t\tj=a+(b*c+1);\r\n\t}\r\n\telse\r\n\t{\r\n\t\tj=a;\r\n\t}\r\n\twhile(i<=100)\r\n\t{\r\n\t\ti=j*2;\r\n\t}\r\n\treturn i;\r\n}\r\n \r\n\r\n" 
+// lexAnalyzer.initLexAnalyzer(code2)
 
-function test() {
-    let result = lexAnalyzer.getLexResult() 
-    console.log(result)
-}
+// function test() {
+//     let result = lexAnalyzer.getLexResult() 
+//     console.log(result)
+// }
 
-test()
+// test()
